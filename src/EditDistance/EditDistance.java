26fe305bed2b7d6747f4a280c58bace5f30f9b0a
PirretKe <zhouke1998301@163.com>
2022-11-03 17:01:32 +0800

    edit distance code

---
+++ src/EditDistance/EditDistance.java
@@ -0,0 +1,34 @@
+package EditDistance;
+
+/**
+ * @author zhouke <zhouke1998301@sjtu.edu.cn>
+ * Created on 2022-11-03
+ * leetcode 72 hard
+ */
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; ++i) {
+            for (int j = 0; j < m + 1; ++j) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                    continue;
+                }
+                if (j == 0) {
+                    dp[i][j] = i;
+                    continue;
+                }
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + 1;
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + 1;
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
